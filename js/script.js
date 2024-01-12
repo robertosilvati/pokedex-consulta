3a7addc3f853687e71fa236597a1f10b966fb683
robertosilvati <roberto.smj.ti@gmail.com>
2024-01-12 16:34:01 -0300

    update: index.html, style.css, script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,80 @@
 const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonImage = document.querySelector(".pokemon__image");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
-}
-
-const renderPokemon = async (pokemon) => { // Tornando a função assíncrona
     try {
-        const data = await fetchPokemon(pokemon);
-        pokemonName.innerHTML = data.name;
-        pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        if (APIResponse.ok) {
+            const data = await APIResponse.json();
+            return data;
+        } else {
+            console.error(`Erro na requisição: ${APIResponse.status}`);
+            return null;
+        }
     } catch (error) {
-        console.error("Erro ao buscar o Pokémon:", error);
+        console.error("Erro na requisição:", error);
+        return null;
     }
 }
 
-renderPokemon(35);
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+
+        // Verifica se data.id é maior ou igual a 649
+        if (data.id >= 649) {
+            // Aplica o estilo para data.id >= 649
+            pokemonImage.classList.add('pokemon__image2');
+        } else {
+            // Remove a classe para voltar ao estilo padrão
+            pokemonImage.classList.remove('pokemon__image2');
+        }
+
+        // Define a imagem com base na condição
+        if (data.id > 0 && data.id < 649) {
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        } else if (data.id >= 649) {
+            pokemonImage.src = data['sprites']['front_default'];
+        }
+
+        input.value = "";
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Sem Resultado';
+        pokemonNumber.innerHTML = "";
+    }
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+    input.value = "";
+});
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon)
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon)
+});
+
+renderPokemon(searchPokemon);
